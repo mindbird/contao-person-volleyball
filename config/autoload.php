3014e02d510d5fd85bb7551b28d2630a5db6c294
Wusch <florian.otto@mindbird.de>
2014-08-29 14:52:53 +0200

    Initial

--- config/autoload.php
+++ config/autoload.php
@@ -12,37 +12,10 @@
 
 
 /**
- * Register the namespaces
- */
-ClassLoader::addNamespaces(array
-(
-	'Person',
-));
-
-
-/**
- * Register the classes
- */
-ClassLoader::addClasses(array
-(
-	// Models
-	'Person\PersonModel'       => 'system/modules/Personen/models/PersonModel.php',
-	'Person\PersonArchivModel' => 'system/modules/Personen/models/PersonArchivModel.php',
-
-	// Modules
-	'Person\ModulePersonList'        => 'system/modules/Personen/modules/ModulePersonList.php',
-	
-	// Elements
-	'Person\PersonContentElement' => 'system/modules/Personen/elments/PersonContentElement.php',
-));
-
-
-/**
  * Register the templates
  */
 TemplateLoader::addFiles(array
 (
-	'ce_person' => 'system/modules/Personen/templates',
-	'mod_personlist' => 'system/modules/Personen/templates',
-	'person_list'    => 'system/modules/Personen/templates',
+	'person_volleyball_list' => 'system/modules/PersonVolleyball/templates',
+	'ce_person_volleyball' => 'system/modules/PersonVolleyball/templates'
 ));
